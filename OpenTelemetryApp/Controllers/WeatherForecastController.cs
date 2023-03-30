4a6191989aef3a5082b745f7620bd5d425bf9fb3
Palienko.an <https://adm-itweb-bitbucket.dns-shop.ru/>
2023-03-31 02:34:39 +0600

    add forecastsCount tag

--- OpenTelemetryApp/Controllers/WeatherForecastController.cs
+++ OpenTelemetryApp/Controllers/WeatherForecastController.cs
@@ -1,3 +1,4 @@
+using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 
 namespace OpenTelemetryApp.Controllers;
@@ -21,12 +22,16 @@ public class WeatherForecastController : ControllerBase
     [HttpGet(Name = "GetWeatherForecast")]
     public IEnumerable<WeatherForecast> Get()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        var activity = Activity.Current;
+        var forecasts = Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
             .ToArray();
+
+        activity?.SetTag("forecastsCount", forecasts.Length);
+        return forecasts;
     }
 }
