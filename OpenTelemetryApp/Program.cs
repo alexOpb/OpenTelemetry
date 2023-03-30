12ad438a80390f093317dfc8035fa6944b282499
Palienko.an <https://adm-itweb-bitbucket.dns-shop.ru/>
2023-03-31 02:31:06 +0600

    add openTelemetry tracing with Jaeger exporter and console output

--- OpenTelemetryApp/Program.cs
+++ OpenTelemetryApp/Program.cs
@@ -1,3 +1,6 @@
+using OpenTelemetry;
+using OpenTelemetry.Trace;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +9,21 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddOpenTelemetryTracing(
+    builder => builder
+        .SetSampler(new AlwaysOnSampler())
+        .AddHttpClientInstrumentation()
+        .AddAspNetCoreInstrumentation()
+        .AddConsoleExporter()
+        .AddJaegerExporter(
+            options =>
+            {
+                options.AgentHost = "localhost";
+                options.AgentPort = 6831;
+                options.ExportProcessorType = ExportProcessorType.Simple;
+            }
+        )
+);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
